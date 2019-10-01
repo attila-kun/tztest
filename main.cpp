6575c5bacad32f1054627d277f41d065db92716c
Attila Kun <kun.attila.hun@gmail.com>
2019-10-02 00:03:25 +0100

    OffsetZone.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,48 @@
 #include "date/tz.h"
 #include <iostream>
+#include <type_traits>
+
+class OffsetZone
+{
+    std::chrono::seconds offset_;
+
+public:
+    explicit OffsetZone(std::chrono::seconds offset)
+        : offset_{offset}
+        {}
+
+    template <class Duration>
+        date::local_time<std::common_type_t<Duration, std::chrono::seconds>>
+        to_local(date::sys_time<Duration> tp) const
+        {
+            using namespace date;
+            using namespace std;
+            using namespace std::chrono;
+            using LT = local_time<common_type_t<Duration, seconds>>;
+            return LT{(tp + offset_).time_since_epoch()};
+        }
+
+    template <class Duration>
+        auto
+        to_sys(date::local_time<Duration> tp) const
+        {
+            using namespace date;
+            using namespace std;
+            using namespace std::chrono;
+            using ST = sys_time<common_type_t<Duration, seconds>>;
+            return ST{(tp - offset_).time_since_epoch()};
+        }
+};
 
 int
 main()
 {
-    using namespace std::chrono_literals;
     using namespace date;
-
-    auto departure = make_zoned("America/New_York", local_days{December/30/1978} + 12h + 1min);
-    auto flight_length = 14h + 44min;
-    auto arrival = make_zoned("Asia/Tehran", departure.get_sys_time() + flight_length);
-
-    std::cout << "departure NYC time:  " << departure << '\n';
-    std::cout << "flight time is       " << make_time(flight_length) << '\n';
-    std::cout << "arrival Tehran time: " << arrival << '\n';
+    using namespace std::chrono;
+    long utcOffset = 60*60*12;
+    OffsetZone p3_45{seconds(utcOffset)};
+    auto hello = local_days(year_month_day(year(2019), month(October), day(1)));
+    zoned_time<milliseconds, OffsetZone*> zt{&p3_45, floor<milliseconds>(hello)};
+    std::cout << zt.get_sys_time() << '\n';
+    std::cout << zt.get_local_time() << '\n';
 }
